a65c5ef35317e42df780981374f6ba9787607395
Diego Vinasco-Alvarez <diego.vinasco@protonmail.com>
2022-12-22 16:54:24 +0100

    update dockerfile and entrypoint to accept arguments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM python:3.8-buster
 
+ARG branch=tags/v1.1.0-alpha
+ARG mount=/inout
+
 # Install dependencies
 RUN apt-get update && apt-get install -q -y \
     openjdk-11-jdk \
-	 git \
+	git \
  && pip install rdflib \
  && pip install lxml \
  && pip install requests
@@ -15,19 +18,20 @@ RUN addgroup --gid $GROUP_ID user \
 	&& adduser --disabled-password --gecos '' --uid $USER_ID --gid $GROUP_ID user
 
 # Setup UD-Graph
-RUN git clone https://github.com/VCityTeam/UD-Graph.git /UD-Graph \
- && cd /UD-Graph \
- && git checkout tags/v1.1.0-alpha
-RUN mkdir /inout \
- && chown -R user:user /inout \
- && chown -R user:user /UD-Graph \
- && cp /UD-Graph/Transformations/ShapeChange/CityGML2.0_config.xml / \
- && cp /UD-Graph/Transformations/ShapeChange/CityGML3.0_config.xml / \
- && cp /UD-Graph/Transformations/XML-to-RDF/citygml_2_namespace_mappings.json / \
- && cp /UD-Graph/Transformations/XML-to-RDF/citygml_3_namespace_mappings.json / \
- && rm -rf /UD-Graph/Transformations/test-data
+RUN git clone --branch ${branch} https://github.com/VCityTeam/UD-Graph.git
+RUN rm -rf /UD-Graph/Transformations/test-data \
+	/UD-Graph/Datasets \
+	/UD-Graph/Ontologies
+## Setup Saxon
+WORKDIR /UD-Graph/Transformations/lib
+RUN wget https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-8J.zip/download
+RUN unzip download \
+ && rm -rf download
 
-# Setup Entrypoint
+# Setup Entrypoint and user permissions
+RUN mkdir ${mount} \
+ && chown -R user:user ${mount} \
+ && chown -R user:user /UD-Graph
 USER user
 COPY entrypoint.py /UD-Graph/Transformations/entrypoint.py
 WORKDIR /UD-Graph/Transformations/
---
+++ entrypoint_old.py
@@ -0,0 +1,38 @@
+import os
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='transformation', help='select transformation pipeline')
+    uml2owl_parser = subparsers.add_parser('shapechange', help='transform a uml model to an owl ontology via ShapeChange')
+    uml2owl_parser.add_argument('config', help='specify the shapechange configuration file')
+    uml2owl_parser.add_argument('--input', help='specify the uml file to transform')
+    uml2owl_parser.add_argument('--output', help='specify a directory to write the transformed model to')
+    xml2rdf_parser = subparsers.add_parser('xml2rdf', help='transform an xml file into a rdf graph')
+    xml2rdf_parser.add_argument('input_file', help='specify the input CityGML datafile')
+    xml2rdf_parser.add_argument('input_model', help='specify the ontology input path; for multiple ontologies, input paths are separated by a ","')
+    xml2rdf_parser.add_argument('mapping_file', help='specify the namespace mapping file.')
+    xml2rdf_parser.add_argument('--format', default='ttl', choices=['ttl', 'rdf'], help='specify the output data format (ttl by default)')
+    xml2rdf_parser.add_argument('--log', default='xml2rdf.log', help='specify the logging file (xml2rdf.log by default)')
+    xsd2owl_parser = subparsers.add_parser('xsd2owl', help='transform an xsd file into an owl ontology')
+    xsd2owl_parser.add_argument('input', help='specify the input xsd file to transform')
+    args = parser.parse_args()
+
+    command = ''
+    if args.transformation == 'shapechange':
+        command = (f"java -jar lib/ShapeChange-2.10.0.jar -Dfile.encoding=UTF-8 " +
+            f"-c /inout/{args.config} -x '$input$' '/inout/{args.input}' -x '$output$' '/inout/{args.output}'")
+    elif args.transformation == 'xml2rdf':
+        command = (f'python XML-to-RDF/XML2RDF.py ' +
+            f'/inout/{args.input_file} /inout/{args.input_model} /inout/{args.mapping_file} --log /inout/{args.log} ' +
+            f'--format {args.format} --output /inout/')
+    elif args.transformation == 'xsd2owl':
+        command = f'cd XSD-to-OWL && python run.py /inout/{args.input} /inout/'
+
+    print(command)
+    os.system(command)
+
+
+if __name__ == "__main__":
+    main()
